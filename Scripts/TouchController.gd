732747094a0a6850947951037ef8b6e94e131c26
MichalKrzyzanowski <c00240696@itcarlow.ie>
2022-06-29 14:21:18 +0100

    bug fixes

--- Scripts/TouchController.gd
+++ Scripts/TouchController.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 # refs
-onready var reactionLabel = get_node("HUD/Label")
+onready var reactionLabel = get_node("HUD/ReactionLabel")
+onready var scoreLabel = get_node("HUD/ScoreLabel")
+onready var timeLeftLabel = get_node("HUD/TimeLeftLabel")
 onready var spawnTimer = get_node("SpawnTimer")
+onready var endTimer = get_node("EndTimer")
 onready var shapeScene = preload("res://Shape/Shape.tscn")
 
 var screenSize := OS.get_window_safe_area()
@@ -16,35 +19,46 @@ var spawnDelay: float
 export var spawnDelayMin := 0.5
 export var spawnDelayMax := 5.0
 
-var waitingForShape := false
+var isWaitingForShape := false
+var isGameOver := false
 
 func _ready() -> void:
 	reactionLabel.text = "Reaction Time: " + str(reactionTime)
+	scoreLabel.text = "Score: " + str(score)
 
 func setReactionTime(newReactionTime: float) -> void:
 	reactionTime = newReactionTime
 	score += 1.0
 	reactionLabel.text = "Reaction Time: " + str(reactionTime)
+	scoreLabel.text = "Score: " + str(score)
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 
 func _process(_delta: float) -> void:
-	if !waitingForShape:
-		instanceShape()
+	if !isGameOver:
+		print("process")
+		timeLeftLabel.text = "Time Left: " + str(endTimer.time_left as int)
+		if !isWaitingForShape:
+			instanceShape()
 	
 func instanceShape():
 	rng.randomize()
 	spawnDelay = rng.randf_range(spawnDelayMin, spawnDelayMax)
 	spawnTimer.wait_time = spawnDelay
 	spawnTimer.start()
-	waitingForShape = true
+	isWaitingForShape = true
 
 func _on_SpawnTimer_timeout() -> void:
-	var newShape = shapeScene.instance()
-	add_child(newShape)
-	newShape.position = Vector2(rng.randf_range(0 + newShape.getRadius() * 2.0, screenSize.size.x - newShape.getRadius() * 2.0),
-	rng.randf_range(0 + newShape.getRadius() * 2.0, screenSize.size.y - newShape.getRadius() * 2.0))
-	newShape.randomModulate(rng)
-	waitingForShape = false
+	if !isGameOver:
+		var newShape = shapeScene.instance()
+		add_child(newShape)
+		newShape.position = Vector2(rng.randf_range(0 + newShape.getRadius() * 2.0, screenSize.size.x - newShape.getRadius() * 2.0),
+		rng.randf_range(0 + newShape.getRadius() * 2.0, screenSize.size.y - newShape.getRadius() * 2.0))
+		newShape.randomModulate(rng)
+		isWaitingForShape = false
+
+
+func _on_EndTimer_timeout() -> void:
+	isGameOver = true
--- Shape/Shape.gd
+++ Shape/Shape.gd
@@ -1,20 +1,31 @@
+tool
 extends Area2D
 
 onready var touchController = get_tree().root.get_node("Root")
 onready var collider = get_node("CollisionShape2D")
 
 var timeElapsed := 0.0
+var isClickable := true
+
+export(Color, RGB) var color = Color.white setget setColor
+
+func setColor(value: Color):
+	color = value
+	modulate = color	
 
 func getRadius() -> float:
 	return collider.shape.radius
 
+func setIsClickable(value: bool):
+	isClickable = value
+
 func randomModulate(rng: RandomNumberGenerator):
-	modulate = Color(rng.randf(), rng.randf(), rng.randf())
+	setColor(Color(rng.randf(), rng.randf(), rng.randf()))
 
 func _process(delta: float) -> void:
 	timeElapsed += delta
 
 func _on_Shape_input_event(_viewport:Node, _event:InputEvent, _shape_idx:int) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") && isClickable:
 		touchController.setReactionTime(timeElapsed)
 		get_tree().queue_delete(self)
